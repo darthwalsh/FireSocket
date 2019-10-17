b089a3a57135d10b3a79f45d1a1b2b25fb1a3667
Carl Walsh <darthwalsh@gmail.com>
2019-10-16 22:14:16 -0700

    Refactor Client to be called FireSocket

--- spec/firesocketspec.js
+++ spec/firesocketspec.js
@@ -1,17 +1,18 @@
 /* global describe it expect */
 
 const Server = require("../server");
-const Client = require("../client");
+const FireSocket = require("../firesocket");
 
 const testing = require("@firebase/testing");
 const firebase = testing.initializeTestApp({
   databaseName: "testDb",
   auth: {uid: "alice"},
 });
+const database = firebase.database();
 
 describe("exiting message", () => {
   it("on server", async done => {
-    await firebase.database().ref().set({user: {user1: {0: "message1"}}});
+    await database.ref().set({user: {user1: {0: "message1"}}});
 
     const server = new Server(firebase);
     const data = await new Promise((res, rej) => {
@@ -26,9 +27,9 @@ describe("exiting message", () => {
   });
 
   it("on client", async done => {
-    await firebase.database().ref().set({server: {user1: {0: "message1"}}});
+    await database.ref().set({server: {user1: {0: "message1"}}});
 
-    const client = new Client("user1", firebase);
+    const client = new FireSocket("user1", firebase);
     const data = await new Promise((res, rej) => {
       client.addEventListener("message", o => res(o.data));
     });
--- spec/helpers/init.js
+++ spec/helpers/init.js
@@ -11,4 +11,5 @@ jasmine.getEnv().addReporter(junitReporter);
 
 const isDebugger = /--debug|--inspect/.test(process.execArgv.join(" "));
 
-jasmine.DEFAULT_TIMEOUT_INTERVAL = isDebugger ? 1000 * 60 * 60 : 1500;
+// 3s is pretty long, but firebase emulators take a long time on cold start
+jasmine.DEFAULT_TIMEOUT_INTERVAL = isDebugger ? 1000 * 60 * 60 : 3000;
